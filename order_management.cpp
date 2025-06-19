9a3d4c54b694c36b3237910ecf8b378571706563
pushkar-gr <gr.pushkar.7@gmail.com>
2025-06-19 20:47:27 +0530

    add: boilerplate code

---
+++ order_management.cpp
@@ -0,0 +1,3 @@
+#include "order_management.hpp"
+
+using namespace std;
---
+++ order_management.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+struct Logon {
+  std::string username;
+  std::string password;
+};
+
+struct Logout {
+  std::string username;
+};
+
+struct OrderRequest {
+  int m_symbolId;
+  double m_price;
+  uint64_t m_qty;
+  char m_side; // possible values 'B' or 'S'
+  uint64_t m_orderId;
+};
+
+enum class RequestType {
+  Unknown = 0,
+  New = 1,
+  Modify = 2,
+  Cancel = 3,
+};
+
+enum class ResponseType {
+  Unknown = 0,
+  Accept = 1,
+  Reject = 2,
+};
+
+struct OrderResponse {
+  uint64_t m_orderId;
+  ResponseType m_responseType;
+};
+
+class OrderManagement {
+  void onData(OrderResponse &&response) {}
+  void send(const OrderRequest &request) {}
+  void sendLogon() {}
+  void sendLogout() {}
+};
